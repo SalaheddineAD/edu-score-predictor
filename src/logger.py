0cfa9bab318920e9fa7448dc32357c59816fdd8e
Salah Eddine Ait Daoud <salaheddineaitdaoud@gmail.com>
2023-07-10 00:05:12 -0500

    add logger

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+# creates a directory at logs_path, exist_ok=True is used so that no error is raised if the folder is already there
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig( 
+    filename=LOG_FILE_PATH, 
+    level=logging.INFO, 
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s'
+    
+    )
