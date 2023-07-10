6ae69e95460244100b99c3b3eb7ef4355c29c1c2
Salah Eddine Ait Daoud <salaheddineaitdaoud@gmail.com>
2023-07-10 00:37:55 -0500

    update and test logger.py and exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -7,12 +8,18 @@ def error_message_detail(error, error_detail: sys):
     error_message = "Error occured in Python script name [{0}], line number [{1}], error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))   
     return error_message
 
-class CustomError(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
     
 
+# Testing if customException is working    
+if __name__=='__main__':
+    try:
+        3/0
+    except Exception as e:
+        raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -13,6 +13,10 @@ LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 logging.basicConfig( 
     filename=LOG_FILE_PATH, 
     level=logging.INFO, 
-    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s'
+    format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s'
     
     )
+
+# just  to test logger.py
+if __name__ == "__main__":
+    logging.info("Logging has started")
